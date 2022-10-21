1c2727510a4deb46c20e5520205c545a5682d32a
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-10-21 20:02:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,8 +26,8 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Al-Botz"//namabot kalian
-global.ownername= "Alam" //nama kalian
+global.botname = "Caca-Canzz"//namabot kalian
+global.ownername= "Rmhdhni" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/haqzzxd" //bebas
@@ -35,15 +35,15 @@ global.email = "haqzganz@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['6285641142178'] //premium user
-global.owner = ["6285964296291","6285641142178"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["6282125312916","6285641142178"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = "628155174757" // nomor wa kalian
 global.ownernomerr = "+6285641142178" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Al-Botzx' //sticker wm ubah
-global.author = 'Di Buat Oleh Al-Botz' //sticker wm ganti nama kalian
+global.packname = '© Caca-Canxz' //sticker wm ubah
+global.author = 'Di Buat Oleh Rmdhni' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
