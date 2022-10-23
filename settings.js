dfd5379b5060097ecb551ab73c136a8267fd33d8
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-10-23 19:12:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,7 +26,7 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "SaxiaBotzV1"//namabot kalian
+global.botname = "SaxiaBotzV2"//namabot kalian
 global.ownername= "Kasan" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
@@ -36,13 +36,13 @@ global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['6285641142178'] //premium user
 global.owner = ["6285641142278","6282110711736" //ganti agar fitur owner bisa di gunakan
-global.ownernomer = ["6283192405363"] // nomor wa kalian
+global.ownernomer = "6283192405363" // nomor wa kalian
 global.ownernomerr = "+6285641142178" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© SaxiaBotzV1'  //sticker wm ubah
+global.packname = '© SaxiaBotzV2'  //sticker wm ubah
 global.author = 'Di Buat Oleh  Saxia-Md' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
